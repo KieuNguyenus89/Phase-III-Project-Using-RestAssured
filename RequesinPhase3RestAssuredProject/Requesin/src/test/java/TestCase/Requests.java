d504657e684468db2bc5ea0657cd285fee4c9fcb
kieunguyenus89 <kieunguyenus89@gmail.com>
2023-05-15 22:04:00 -0400

    first commit

---
+++ RequesinPhase3RestAssuredProject/Requesin/src/test/java/TestCase/Requests.java
@@ -0,0 +1,39 @@
+package TestCase;
+
+import static io.restassured.RestAssured.given;
+
+import org.apache.log4j.xml.DOMConfigurator;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.restassured.response.Response;
+import resources.Log4j;
+
+public class Requests {
+
+	String baseURL = "https://reqres.in";
+
+	@Test
+	public void getMethodwithPathParam() {
+
+		DOMConfigurator.configure("log4j.xml");
+		Log4j.startTestCase("getMethodwithPathParam");
+		Response res = given().when().get(baseURL + "/api/users?page=2");
+		Assert.assertEquals(res.getStatusCode(), 200);
+
+		System.out.println(res.asPrettyString());
+		Log4j.info(res.asPrettyString());
+
+	}
+
+	@Test
+	public void PostMethod() {
+		DOMConfigurator.configure("log4j.xml");
+		Log4j.startTestCase("PostMethod");
+		Response res = given().header("Content-Type", "application/json")
+				.body("{\"name\":\"morpheus\",\"job\":\"leader\"}").when().post("https://reqres.in/api/users");
+		System.out.println(res.asPrettyString());
+		Log4j.info(res.asPrettyString());
+	}
+
+}
